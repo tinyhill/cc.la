4d062e87537d964ca2e98632c127b99728a40d9b
tinyhill <tinyhill@163.com>
2015-01-29 02:10:32 +0800

    version dump

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
