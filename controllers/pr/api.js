63cf9b693dcb6d099409ea9547c14ea0ac55c391
tinyhill <tinyhill@163.com>
2015-02-06 11:40:45 +0800

    更新 PR API

---
+++ controllers/pr/api.js
@@ -0,0 +1,29 @@
+var PageRank = require('pagerank');
+var parseDomain = require('parse-domain');
+
+exports.index = function (req, res) {
+
+    var q = parseDomain(req.params.q);
+
+    if (q) {
+        PageRank.HOST = 'toolbarqueries.google.com.hk';
+        PageRank.get(q, function (err, data) {
+            if (err) {
+                res.send({
+                    status: 'fail',
+                    data: err
+                });
+            } else {
+                res.send({
+                    status: 'success',
+                    data: data
+                });
+            }
+        });
+    } else {
+        res.send({
+            status: 'error',
+            message: q
+        });
+    }
+};
