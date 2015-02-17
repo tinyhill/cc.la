030a184872ccded2502d4c4a48e3832c5a03aca4
tinyhill <tinyhill@163.com>
2015-02-17 23:14:47 +0800

    version dump

---
+++ controllers/IpController.js
@@ -0,0 +1,26 @@
+var parseDomain = require('parse-domain');
+
+exports.index = function (req, res) {
+
+    var q = req.params.q || req.query.q || '';
+    var data = {
+        active: 'ip',
+        layout: 'layouts/default'
+    };
+
+    if (q) {
+
+        var parsed = parseDomain(q);
+
+        if (parsed) {
+            data.q = parsed.domain + '.' + parsed.tld;
+            res.render('ip', data);
+        } else {
+            data.body = '请输入正确的网址';
+            res.render('ip', data);
+        }
+    } else {
+        data.body = '请输入要查询的网址';
+        res.render('ip', data);
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var IndexController = require('../controllers/IndexController');
+var IpController = require('../controllers/IpController');
 var PrController = require('../controllers/PrController');
-var SiteController = require('../controllers/SiteController');
 var WhoisController = require('../controllers/WhoisController');
 
 router.get('/', IndexController.index);
+router.get('/ip', IpController.index);
+router.get('/ip/:q', IpController.index);
 router.get('/pr', PrController.index);
 router.get('/pr/:q', PrController.index);
-router.get('/site', SiteController.index);
-router.get('/site/:q', SiteController.index);
 router.get('/whois', WhoisController.index);
 router.get('/whois/:q', WhoisController.index);
 
