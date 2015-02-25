d0ccec233ccb7af02dd07b3cd63a6dc0dd61a415
tinyhill <tinyhill@163.com>
2015-02-25 20:15:04 +0800

    req.cookies.q

--- controllers/AboutController.js
+++ controllers/AboutController.js
@@ -2,7 +2,8 @@ exports.index = function (req, res) {
     res.render('about', {
         active: 'about',
         layout: 'layouts/about_default',
-        title: '关于本站'
+        title: '关于本站',
+        q: req.cookies.q
     });
 };
 
@@ -10,7 +11,8 @@ exports.contact = function (req, res) {
     res.render('about/contact', {
         active: 'contact',
         layout: 'layouts/about_default',
-        title: '联系我们'
+        title: '联系我们',
+        q: req.cookies.q
     });
 };
 
@@ -18,7 +20,8 @@ exports.help = function (req, res) {
     res.render('about/help', {
         active: 'help',
         layout: 'layouts/about_default',
-        title: '帮助中心'
+        title: '帮助中心',
+        q: req.cookies.q
     });
 };
 
@@ -26,6 +29,7 @@ exports.links = function (req, res) {
     res.render('about/links', {
         active: 'links',
         layout: 'layouts/about_default',
-        title: '友情链接'
+        title: '友情链接',
+        q: req.cookies.q
     });
 };
