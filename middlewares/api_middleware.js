2971fa8530d903bdff1cc4e867fbdf24e3fdba61
tinyhill <tinyhill@163.com>
2015-04-12 11:18:01 +0800

    version dump

--- middlewares/api_middleware.js
+++ middlewares/api_middleware.js
@@ -1,6 +1,9 @@
 exports.index = function (req, res, next) {
-
-    next();
-
-    // todo
+    if (req['X-Requested-With'] && req['X-Requested-With'] === 'XMLHttpRequest') {
+        next();
+    } else {
+        res.status(401).render('error', {
+            message: 'Unauthorized'
+        });
+    }
 };
