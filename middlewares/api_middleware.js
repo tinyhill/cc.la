d0a44de0f83484483a742c542fc83a020dfc42e0
tinyhill <tinyhill@163.com>
2015-04-15 17:42:03 +0800

    version dump

--- middlewares/api_middleware.js
+++ middlewares/api_middleware.js
@@ -2,8 +2,9 @@ exports.index = function (req, res, next) {
 
     var headers = req.headers;
     var xRequestedWith = headers['x-requested-with'] && headers['x-requested-with'] === 'XMLHttpRequest';
+    var isLocal = req.hostname === '127.0.0.1';
 
-    if (xRequestedWith && headers.referer) {
+    if (xRequestedWith && headers.referer || isLocal) {
         next();
     } else {
         res.status(403).send(null);
